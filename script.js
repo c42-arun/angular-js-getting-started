0a9c17d2daf003a2926ac9ad0dd56c8e1bc4d090
c42-arun <arun.kumar.rs@gmail.com>
2018-09-20 15:41:25 +0100

    ng-repeat

--- script.js
+++ script.js
@@ -6,17 +6,32 @@
 
 
   var MainController = function($scope, $http) {
-    
+
+    $scope.message = "GitHub Viewer";
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+
+      // get user repos
+      $http.get($scope.user.repos_url)
+        .then(onReposComplete, onError);
+    };
+
+    var onReposComplete = function(response) {
+      $scope.repos = response.data;
+    };
+
+    var onError = function(reason) {
+      $scope.error = 'Could not get data';
+    };
+
     $scope.search = function(username){
       $http.get('https://api.github.com/users/' + username)
-        .then((response) => {
-          $scope.github = response.data;
-        }, (error) => {
-          $scope.error = 'Could not get user details';
-      });
+        .then(onUserComplete, onError);
     };
   }
   
-  app.controller("MainController", MainController);
+  // passing parameters explicitly so angular knows what they are after mangled by minification
+  app.controller("MainController", ["$scope", "$http", MainController]);
   
 }());
